28d71b8ff2914657cd5199383563dbb5e626b535
Randeep <randeep1810@gmail.com>
2017-06-22 10:06:17 +0530

    updations in user service class

--- userservice/src/main/java/com/stackroute/gupshup/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/stackroute/gupshup/userservice/UserserviceApplication.java
@@ -1,54 +1,21 @@
 package com.stackroute.gupshup.userservice;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.web.WebMvcProperties.LocaleResolver;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
-
 
 import com.stackroute.gupshup.userservice.consumer.UserConsumer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableMongoRepositories
-@EnableWebMvc
-public class UserserviceApplication extends WebMvcConfigurerAdapter {
+public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(UserserviceApplication.class, args);
 		applicationContext.getBean(UserConsumer.class).subscribeUserActivity("testuserkafka");
 	}
 	
-	@Bean
-	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setDefaultLocale(Locale.US);
-	    return slr;
-	}
-	
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-	    lci.setParamName("lang");
-	    return lci;
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
-	}
 }
