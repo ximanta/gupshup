7e88a596eccd5688fc177cae919b34a5557ff1ac
ayushee18 <agarwalayushee09@gmail.com>
2017-06-28 14:51:27 +0530

    added kafka consumer

--- recommendationservice/src/main/java/com/stackroute/gupshup/recommendationservice/service/UserRecommendationService.java
+++ recommendationservice/src/main/java/com/stackroute/gupshup/recommendationservice/service/UserRecommendationService.java
@@ -1,33 +1,13 @@
 package com.stackroute.gupshup.recommendationservice.service;
 
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.stackroute.gupshup.recommendationservice.entity.UserRecommendation;
-import com.stackroute.gupshup.recommendationservice.repository.UserRecommendationRepository;
 
-@Service
-public class UserRecommendationService {
+public interface UserRecommendationService {
 	
-	@Autowired
-	UserRecommendationRepository userRecommendationRepository;
-	
-	public Map<String, Object> createUser(UserRecommendation userRecommendation){
-				return userRecommendationRepository.createUser(
-				userRecommendation.getName(),
-				userRecommendation.getFirstname(),
-				userRecommendation.getLastname(),
-				userRecommendation.getGender(),
-				userRecommendation.getIntrest(),
-				userRecommendation.getDOB());
-	}
-	
-	public Iterable<Map<String, Object>> follows(String user1, String user2){
-		System.out.println("service "+user1+" "+user2);
-		return userRecommendationRepository.follows(user1, user2);
-	}
-	
+	public Map<String, Object> createUser(UserRecommendation userRecommendation);
+	public Iterable<Map<String, Object>> follows(String user1, String user2);
+	public void getActivityType(JsonNode node);
 }
