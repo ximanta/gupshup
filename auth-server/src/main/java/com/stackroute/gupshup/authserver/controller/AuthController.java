f9571410a56985d7984556b36081f1456f0f2265
Amardeep Kumar <amardeepssgj@gmail.com>
2017-07-11 09:21:49 +0530

    modified auth-server

--- auth-server/src/main/java/com/stackroute/gupshup/authserver/controller/AuthController.java
+++ auth-server/src/main/java/com/stackroute/gupshup/authserver/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,6 @@ public class AuthController {
 	
 	@RequestMapping("/register")
 	public ResponseEntity<User> register(@RequestBody User user){
-		System.out.println(user);
 		user.setRoles("USER");
 		User savedUser = userRepository.save(user);
 		return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
